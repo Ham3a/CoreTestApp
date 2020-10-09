b90fa34aacd2d412c55c661af07d516bce3c4759
Ham3a <61283002+Ham3a@users.noreply.github.com>
2020-10-09 16:56:21 +0100

    Add project files.

---
+++ CoreTestApp/Pages/HelloWorld.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Primitives;
+
+namespace CoreTestApp.Pages
+{
+    public class HelloWorldModel : PageModel
+    {
+        public StringValues name;
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            this.name = Request.Form["Name"];
+        }
+    }
+}
